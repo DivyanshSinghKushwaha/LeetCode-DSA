3e71b80c6c9d090fa11387d8104c426547729283
Divyansh Singh Kushwaha <divyanshsinghkushwaha@gmail.com>
2026-08-23 21:38:36 +0530

    Time: 0 ms (100%), Space: 48.5 MB (31.5%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int beg,mid,end;
+        beg=0;
+        end=nums.length-1;
+        while(beg<=end){
+            mid = beg + (end-beg)/2;
+            if(nums[mid]<target){
+                beg=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
